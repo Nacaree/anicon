367c45c0ea5d49456d67f60cd7048c772b6af712
Nacare <nacare13g@gmail.com>
2026-01-24 17:31:29 +0700

    removed NextJs default font

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -2,16 +2,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "AniCon",
   description: "The best Anime Community in Cambodia",
@@ -20,10 +10,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Providers>
-          {children}
-        </Providers>
+      <body>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
